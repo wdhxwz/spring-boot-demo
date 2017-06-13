c681e267405fb09479ca432f8cf74d6e9198e1b2
王东鸿 <1366678737@qq.com>
2017-06-14 00:46:47 +0800

    添加jsr 303 验证

--- Hello-world/src/main/java/com/spring/boot/demo/apiservice/ApiRequest2001.java
+++ Hello-world/src/main/java/com/spring/boot/demo/apiservice/ApiRequest2001.java
@@ -1,19 +1,30 @@
 package com.spring.boot.demo.apiservice;
 
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.spring.boot.demo.apirouter.ApiRequest;
 
 /**
  * 查询指定渠道下用户的账户列表
+ * 
  * @author wdhcxx
  *
  */
-public class ApiRequest2001 extends ApiRequest{
+public class ApiRequest2001 extends ApiRequest {
 	private static final long serialVersionUID = 16046829195261061L;
-	
+
 	/**
 	 * 渠道编号
 	 */
+
+	@NotNull
 	private String channelNo;
+	
+	@NotNull
+	@Length(min=10,max=20)
+	private String name;
 
 	public String getChannelNo() {
 		return channelNo;
@@ -21,5 +32,13 @@ public class ApiRequest2001 extends ApiRequest{
 
 	public void setChannelNo(String channelNo) {
 		this.channelNo = channelNo;
-	}		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
