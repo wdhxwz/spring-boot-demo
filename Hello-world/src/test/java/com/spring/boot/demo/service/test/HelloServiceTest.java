1b25081d9ebdeef66bcef02481002f86c8c178df
王东鸿 <1366678737@qq.com>
2017-06-14 22:22:58 +0800

    去掉dubboapi测试

--- Hello-world/src/test/java/com/spring/boot/demo/service/test/HelloServiceTest.java
+++ Hello-world/src/test/java/com/spring/boot/demo/service/test/HelloServiceTest.java
@@ -5,24 +5,13 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.spring.boot.demo.service.HelloService;
-import com.wangdh.learner.dubbo.api.ISampleService;
 
 public class HelloServiceTest extends TestBase {
 	@Autowired
 	private HelloService helloService;
 	
-	@Autowired
-	private ISampleService sampleService;
-	
 	@Test
 	public void sayHelloTest(){
 		Assert.assertEquals("hello world", helloService.sayHello());
-	}
-	
-	@Test
-	public void dubboTest(){
-		String response = sampleService.sayHello("spring boot");
-		System.out.println(response);
-		Assert.assertNotNull(response);
 	}
 }
