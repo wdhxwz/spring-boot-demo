a3879338e9645007daa6e82d510720f8a36adfb2
王东鸿 <wangdonghong@glexer.com>
2018-01-17 17:38:09 +0800

    Springboot quickstart

---
+++ springboot-quickstart/src/main/java/com/wangdh/springboot/quickstart/controller/Example.java
@@ -0,0 +1,17 @@
+package com.wangdh.springboot.quickstart.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangdh
+ * @Description
+ * @date 2018-01-17
+ */
+@RestController
+public class Example {
+    @RequestMapping("/")
+    public  String home(){
+        return "Hello Spring Boot";
+    }
+}
